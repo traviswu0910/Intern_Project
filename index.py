5eb75807437931c794539bbb14151e08bd60c929
traviswu0910 <a0979030620@gmail.com>
2020-07-07 15:16:39 +0800

    Index problem

--- index.py
+++ index.py
@@ -21,6 +21,7 @@ def login():
         global user
         user = request.values['usr']
         pwd = request.values['pwd']
+        data = []
         try:
             with open(f'./All_Data/Reference/Info.json', 'r') as json_file:
                 data = json.load(json_file)
@@ -71,9 +72,10 @@ def op():
         year = request.values['datepicker']
         portfolio = request.values['portfolio']
         keyword = request.form['ikeyword']
+        data = []
         try:
             with open(f'./All_Data/Reference/Info.json', 'r') as json_file:
-            data = json.load(json_file)
+                data = json.load(json_file)
         except:
             with open(f'./All_Data/Reference/Info.json', 'w+') as f:
                 json.dump([], f)
@@ -87,7 +89,7 @@ def op():
                                     "pf" : portfolio,
                                     "kw" : keyword }})
         with open(f'./All_Data/Reference/Info.json', 'w') as json_file:
-           json.dump(data, json_file)
+            json.dump(data, json_file)
         selected = {'pph_1':'','pph_2':'','pph_3':'','pph_4':''}
         selected[portfolio]='selected'
         date = pd.to_datetime(year).strftime('%m/%d/%Y')
@@ -122,7 +124,7 @@ def op():
 
 def get_top_news(which_day,num,keyword):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
-    with open(f'./All_Data/UIData/news/{which_day}_{num}.json')as f:
+    with open(f'./All_Data/top_news/{which_day}_{num}.json')as f:
         file = json.load(f)
         key = file[0]
         news = file[1:]
@@ -144,7 +146,7 @@ def get_portfolio_news(which_day,method,keyword):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
     method = method_list[method]
     try:
-        with open(f'./All_Data/UIData/news/{method}_{which_day}.json')as f:
+        with open(f'./All_Data/portfolio_news/{method}_{which_day}.json')as f:
             file = json.load(f)
         if len(file)>1:
             portfolio = file[0]
@@ -175,7 +177,7 @@ def get_portfolio_news(which_day,method,keyword):
 def get_chart_data(which_day,method):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
     method = method_list[method].replace('news_PortfolioList_','')
-    data = pd.read_json(f'./All_Data/UIData/chart/PortfolioPerformance_{method}_{which_day}.json')
+    data = pd.read_json(f'./All_Data/UIData/PortfolioPerformance_{method}_{which_day}.json')
     data['company'] = data['InfoCode'].apply(lambda x:fullName.loc[int(x)][0])
     data['Single']=data['Single']*360
     data=data.rename(columns={'Single':'day','Nearest7DaysAnnualSingle':'week',
@@ -209,18 +211,18 @@ def get_chart_data(which_day,method):
 
 def get_top_twitter(which_day,num):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
-    with open(f'./All_Data/UIData/twitter/{which_day}_{num}.json')as f:
+    with open(f'./All_Data/top_twitters/{which_day}_{num}.json')as f:
         file = json.load(f)
         key = file[0]
         twitter = file[1:]
     return key,twitter
 def get_hot_twitter(which_day):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
-    with open(f'./All_Data/UIData/twitter/FundyLongShort+{which_day}.json')as f:
+    with open(f'./All_Data/top_author_twitters/FundyLongShort+{which_day}.json')as f:
         file1 = json.load(f)
-    with open(f'./All_Data/UIData/twitter/SmallCapLS+{which_day}.json')as f:
+    with open(f'./All_Data/top_author_twitters/SmallCapLS+{which_day}.json')as f:
         file2 = json.load(f)
-    with open(f'./All_Data/UIData/twitter/ShortSightedCap+{which_day}.json')as f:
+    with open(f'./All_Data/top_author_twitters/ShortSightedCap+{which_day}.json')as f:
         file3 = json.load(f)
     return file1,file2,file3
         
