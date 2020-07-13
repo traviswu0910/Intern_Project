33a4c5e8be36f5b917f3a018d8b6c11f3a40a69a
traviswu0910 <a0979030620@gmail.com>
2020-07-13 15:54:01 +0800

    add

--- News_Crawler_WSJ.py
+++ News_Crawler_WSJ.py
@@ -39,8 +39,8 @@ headers = {
 print(headers)#檢查哪個user agent可以哪個不行
 today = str(date.today())
 today_for_crawl = str(date.today()).replace('-', '')
-target = '20200709' #要抓的日期
-date_for_pubdate = '2020-07-09'
+target = '20200711' #要抓的日期
+date_for_pubdate = '2020-07-11'
 url='https://www.wsj.com/news/archive/'+target
 print(target)
 
@@ -75,7 +75,7 @@ for news in news_list:
 	data.append(dic)
 
 
-with open(f'./All_Data/News_ParsedData/2020-07-09_WSJ.json','w') as f:
+with open(f'./All_Data/News_ParsedData/2020-07-11_WSJ.json','w') as f:
 	json.dump(data,f)
 
 
